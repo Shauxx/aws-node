64206ff8c81fe2be5388dee95160d31f71dc0bde
shau vargas <Vargasshau@gmail.com>
2024-05-23 20:48:24 -0600

    segunda

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Hello, world!'));
 
-app.listen(3000);
-console.log('Server on port 3000');
+app.listen(8080);
+console.log('Server on port 8080');
